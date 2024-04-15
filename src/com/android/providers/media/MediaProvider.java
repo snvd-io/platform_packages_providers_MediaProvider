0d93a095de19faf32fa51aa6ac27f58f0eec8d82
Omar Eissa <oeissa@google.com>
2024-04-15 12:04:56 +0000

    Prevent insertion in other users storage volumes
    Don't allow file insertion in other users storage volumes.
    This was already handled if DATA was explicitly set in content values,
    but was allowed if DATA was generated based on other values like RELATIVE_PATH and DISPLAY_NAME.
    
    Insertion of files in other users storage volumes can be used by malicious apps
    to get access to other users files, since the same file would exist in both users MP databases
    which would lead to MP falsely assuming that the user has access to this file.
    
    Bug: 294406604
    Test: atest MediaProviderTests
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:df39f8486b25473d0bdbeed896ad917e3c793bf9)
    Merged-In: Ie219bbdbe28819421040e4c083b65ab47d8ebde6
    Change-Id: Ie219bbdbe28819421040e4c083b65ab47d8ebde6

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -4525,6 +4525,7 @@ public class MediaProvider extends ContentProvider {
 
             FileUtils.sanitizeValues(values, /*rewriteHiddenFileName*/ !isFuseThread());
             FileUtils.computeDataFromValues(values, volumePath, isFuseThread());
+            assertFileColumnsConsistent(match, uri, values);
 
             // Create result file
             File res = new File(values.getAsString(MediaColumns.DATA));
